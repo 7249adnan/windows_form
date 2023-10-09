358376a9b0e8067a66e81b6d43e851e07053a935
Adnan Shaikh <53960360+7249adnan@users.noreply.github.com>
2023-10-09 12:20:04 +0530

    Add files via upload

---
+++ Media Player by adnan/ListBox_1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ListBox_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.mdp1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.fd1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mdp1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(444, 430);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(71, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(395, 149);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(340, 225);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(254, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = " &Upload ==>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(254, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<=== Take";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(71, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Choose &File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mdp1
+            // 
+            this.mdp1.Enabled = true;
+            this.mdp1.Location = new System.Drawing.Point(780, 79);
+            this.mdp1.Name = "mdp1";
+            this.mdp1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mdp1.OcxState")));
+            this.mdp1.Size = new System.Drawing.Size(309, 359);
+            this.mdp1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(439, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Video List Box";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(826, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Video Player";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(356, 432);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(203, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 40);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Media Player";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Navy;
+            this.label5.Location = new System.Drawing.Point(878, 497);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(268, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "( Created By Adnan Shaikh on 7th October 2023 )";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 521);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mdp1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.mdp1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog fd1;
+        private AxWMPLib.AxWindowsMediaPlayer mdp1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Media Player by adnan/ListBox_1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ListBox_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Blue;
+
+            if (listBox1.SelectedItem != null)
+            {
+                textBox1.Text = " Uploading video From List 1 to 2 ";
+
+                String tempI = listBox1.SelectedItem.ToString();
+
+               int index= listBox1.SelectedIndex;
+              
+
+                int total = listBox1.Items.Count;
+
+             
+
+                if (total > 1 && index< total-1)
+                {
+                    listBox1.SelectedIndex = index + 1;
+                }
+                else if ((index+1) == total){
+
+                    listBox1.SelectedIndex = index-1;
+                }
+                else
+                {
+                    listBox1.SelectedIndex = index;
+                }
+                
+
+                listBox1.Items.Remove(tempI); 
+                listBox2.Items.Add(tempI);
+
+            }
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+
+            textBox1.ForeColor = Color.Blue;
+
+            if (listBox2.SelectedItem != null)
+            {
+                String tempI = listBox2.SelectedItem.ToString();
+
+                int index = listBox2.SelectedIndex;
+
+
+                int total = listBox2.Items.Count;
+
+              
+
+                if (total > 1 && index < total - 1)
+                {
+                    listBox2.SelectedIndex = index + 1;
+                }
+                else if ((index + 1) == total)
+                {
+                   
+
+                    listBox2.SelectedIndex = index - 1;
+                   
+                }
+                else
+                {
+                    listBox2.SelectedIndex = index;
+                }
+
+                textBox1.Text = " Taking video From List 2 to 1 ";
+
+                listBox2.Items.Remove(tempI);
+                listBox1.Items.Add(tempI);
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (fd1.ShowDialog() == DialogResult.OK){
+
+                textBox1.Text = "File Choosed Successfully ";
+                textBox1.ForeColor = Color.Green;
+                listBox1.Items.Add(fd1.FileName);
+              
+            }
+            else
+            {
+                textBox1.Text = " Choose Correct file  ";
+                textBox1.ForeColor = Color.Red;
+            }
+
+
+        }
+        int temp = 0;
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+         
+
+
+            if (listBox2.SelectedItem != null)
+            {
+
+                String tempI2 = listBox2.SelectedItem.ToString();
+                textBox1.ForeColor = Color.Blue;
+
+               
+                string lastFour = tempI2.Substring(tempI2.Length - 4);
+
+               
+
+
+
+                textBox1.Text = lastFour;
+
+                if (lastFour == ".mp4")
+                {
+                    mdp1.URL = tempI2;
+                    mdp1.Ctlcontrols.play();
+                    textBox1.Text = "Playing Current Selected File ";
+                    temp = 0; 
+                }
+                else
+                {   
+                    listBox2.Items.Remove(tempI2);
+                }
+                
+
+            }
+            else
+            {
+               
+                    textBox1.Text = " Choose Correct file with .mp4 Extension  ";
+                    textBox1.ForeColor = Color.Red;
+
+            }
+
+
+           
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+    }
+}
